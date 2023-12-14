3758ece421df083b586b9a9bb025063a96c73dfd
webbalaka <webbalaka@gmail.com>
2023-12-14 21:19:53 +0700

    FetchAndDrawTable

--- Backend/app.js
+++ Backend/app.js
@@ -18,9 +18,18 @@ const con = mysql.createConnection({
 app.post("/submit", async (req, res)=> {
   const item = req.body;
   const sql = "INSERT INTO comments (Comment, Author, UserId) VALUES (" + `"${item.Comment}", "${item.Author}", "${item.UserId}")`;
-  con.query(sql, function(err, result){
+  await con.query(sql,  (err, result)=>{
     if(err) throw err;
     console.log(result);
   })
+  res.sendStatus(200);
 });
+
+app.get("/get", async(req, res)=> {
+  const sql = "SELECT * FROM comments";
+  con.query(sql, (err, result)=>{
+    if(err) throw err;
+    res.status(200).json(result);
+  })
+})
 export default app;
--- Frontend/public/script/index.js
+++ Frontend/public/script/index.js
@@ -1,40 +1,57 @@
-import {CreateItem} from "./api.js"
+import {CreateItem, GetItem} from "./api.js"
 let butTag = document.createElement("button");
 
+document.addEventListener("DOMContentLoaded", ()=>{
+    FetchAndDrawTable();
+})
 
-Fsub.addEventListener("submit", (e) => {
+
+
+async function FetchAndDrawTable(){
+    const item = await GetItem();
+    DrawTable(item);
+}
+
+function DrawTable(item){
+    Content.innerHTML = "";
+    for(const items of item){
+        console.log(items);
+        const textNode = document.createTextNode(items.Comment);
+        const divTag = document.createElement("div");
+        divTag.className = "comment_row";
+        const pTag = document.createElement("p");
+        const divPTag = document.createElement("div");
+        divPTag.className = "comment_content";
+        butTag = document.createElement("button");
+        butTag.innerHTML = "Edit";
+        
+        butTag.onclick = ()=>{
+            Edit(items.Comment);
+        };
+
+        pTag.appendChild(textNode);
+        divPTag.appendChild(pTag);
+        divTag.appendChild(divPTag);
+        divTag.appendChild(butTag);
+        Content.appendChild(divTag);
+    }
+}
+
+ Fsub.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const textNode = document.createTextNode(data.value);
-    const divTag = document.createElement("div");
-    divTag.className = "comment_row";
-    const pTag = document.createElement("p");
-    const divPTag = document.createElement("div");
-    divPTag.className = "comment_content";
-    butTag = document.createElement("button");
-    butTag.innerHTML = "Edit";
-    
-    butTag.onclick = ()=>{
-        Edit(textNode);
-    };
-
-    pTag.appendChild(textNode);
-    divPTag.appendChild(pTag);
-    divTag.appendChild(divPTag);
-    divTag.appendChild(butTag);
-    Content.appendChild(divTag);
-
-    push(data.value);
+    await Push(data.value)
     data.value = "";
 })
 
-function push(text){
+async function Push(text){
     const payLoad = {
         Comment: text,
         Author: "USER1",
         UserId: "6633162221"
     }
-    console.log(payLoad);
-    CreateItem(payLoad);
+    // console.log(payLoad);
+    await CreateItem(payLoad);
+    await FetchAndDrawTable();
 }
 
 
